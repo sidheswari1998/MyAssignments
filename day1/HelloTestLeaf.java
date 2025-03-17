bbfeba9ce83d6117d3c407e9438ba43f7a63f1e4
sidheswari1998 <sidheswari1998bvn@gmail.com>
2025-03-17 16:13:36 +0530

    Add files via upload

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,12 @@
+package week.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My First Code Executed");
+
+	}
+
+}
